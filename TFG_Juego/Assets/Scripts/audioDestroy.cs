4f4d6021a723cf9c626f0f049df6f9e0642aaeb5
avale99 <alexava99@gmail.com>
2022-06-06 12:55:48 +0200

    Musica menus y Musica en batalla

---
+++ TFG_Juego/Assets/Scripts/audioDestroy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class audioDestroy : MonoBehaviour
+{
+    void Awake(){
+        if (SceneManager.GetActiveScene().name == "firstLevel"){  
+            GameObject.Find("Music").GetComponent<AudioSource>().Stop();
+        }else{
+            DontDestroyOnLoad(transform.gameObject);
+        }
+    }
+
+    
+
+    
+}
